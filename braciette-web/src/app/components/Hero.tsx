536f69910d517dde9ec5d84485b0923ca56f6170
tristan <tristan.alhabas@gmail.com>
2025-09-04 15:40:35 +0700

    work: Local Font

---
+++ braciette-web/src/app/components/Hero.tsx
@@ -0,0 +1,10 @@
+function Hero() {
+  return (
+    <section>
+      <h1 className="font-extrabold font-retro">Welcome to Braciette</h1>
+      <p>Your one-stop solution for all things delicious.</p>
+    </section>
+  );
+}
+
+export default Hero;
