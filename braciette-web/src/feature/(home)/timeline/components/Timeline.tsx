70ad8f90ce81e529d116911c65108ed1ed6f0ecb
tristan <tristan.alhabas@gmail.com>
2025-09-17 19:44:45 +0700

    feat: Timeline Section Responsive

--- braciette-web/src/feature/(home)/timeline/components/Timeline.tsx
+++ braciette-web/src/feature/(home)/timeline/components/Timeline.tsx
@@ -1,23 +1,63 @@
 import Image from "next/image";
 import style from "../styles/Timeline.module.css";
+import { title } from "process";
 
 function Timeline() {
+  const timelines: Array<{
+    id: number;
+    title: string;
+    date: string;
+    desc: string;
+    position: string;
+  }> = [
+    {
+      id: 1,
+      title: "United Rhythm UKM Parade",
+      date: "10 September 2025",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      position: "left",
+    },
+    {
+      id: 2,
+      title: "United Rhythm UKM Parade",
+      date: "10 September 2025",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      position: "right",
+    },
+    {
+      id: 3,
+      title: "United Rhythm UKM Parade",
+      date: "10 September 2025",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      position: "left",
+    },
+    {
+      id: 4,
+      title: "United Rhythm UKM Parade",
+      date: "10 September 2025",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      position: "right",
+    },
+  ];
   return (
     <section className="flex flex-col justify-center items-center bg-blue">
       <div className="w-full h-full z-40 bg-blue flex flex-col justify-center items-center px-8 md:px-20 2xl:px-30 py-[20dvh]">
         <div className="relative flex flex-row justify-center items-center gap-24">
-          <div className="absolute -left-60 top-0">
+          <div className="hidden md:block absolute -left-40 top-0">
             <Image
               src="/landing-page/3arrow.svg"
               alt="Arrows"
-              width={150}
-              height={150}
+              width={100}
+              height={100}
             />
           </div>
           <div className="flex flex-col justify-center items-center">
-            <h1 className="relative font-yourmate text-green text-[120px]">
+            <h1
+              className="relative font-yourmate text-green"
+              style={{ fontSize: "clamp(56px, 6vw + 1rem, 120px)" }}
+            >
               TIMELINE
-              <div className="absolute -top-15 -right-45">
+              <div className="hidden md:block absolute -top-15 -right-45">
                 <Image
                   src="/landing-page/timeline-element.svg"
                   alt="Decorative Element"
@@ -26,68 +66,37 @@ function Timeline() {
                 />
               </div>
             </h1>
-            <h2 className="font-retro text-pink text-[96px] -mt-10">
+            <h2
+              className="font-retro text-pink text-[96px] -mt-5 xl:-mt-10"
+              style={{ fontSize: "clamp(46px, 5vw, 96px)" }}
+            >
               BRACIATE 2025
             </h2>
           </div>
         </div>
         <div className={`${style.timeline}`}>
-          <div className={`${style.container} ${style.leftContainer}`}>
+          {timelines.map((timeline) => (
             <div
-              className={`w-[50px] h-[50px] rounded-full bg-white ${style.balls}`}
-            />
-            <div className={`${style.textBox}`}>
-              <h2 className="font-retro text-2xl">UNITED RHTYMS UKM PARADE</h2>
-              <small className="font-poppins">10 September 2025</small>
-              <p className="font-poppins mt-3">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-              </p>
-              <span className={`${style.arrowLeft}`}></span>
+              key={timeline.id}
+              className={`${style.container} ${
+                style[`${timeline.position}Container`]
+              }`}
+            >
+              <div
+                className={`w-[50px] h-[50px] rounded-full bg-yellow ${style.balls}`}
+              />
+              <div className={`${style.textBox}`}>
+                <h2 className="font-retro text-4xl text-blue">
+                  {timeline.title}
+                </h2>
+                <small className="font-poppins">{timeline.date}</small>
+                <p className="font-poppins mt-4">{timeline.desc}</p>
+                <span
+                  className={`${style[`${timeline.position}Arrow`]}`}
+                ></span>
+              </div>
             </div>
-          </div>
-          <div className={`${style.container} ${style.rightContainer}`}>
-            <div
-              className={`w-[50px] h-[50px] rounded-full bg-white ${style.balls}`}
-            />
-            <div className={`${style.textBox}`}>
-              <h2 className="font-retro text-2xl">UNITED RHTYMS UKM PARADE</h2>
-              <small className="font-poppins">10 September 2025</small>
-              <p className="font-poppins mt-3">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-              </p>
-              <span className={`${style.arrowRight}`}></span>
-            </div>
-          </div>
-          <div className={`${style.container} ${style.leftContainer}`}>
-            <div
-              className={`w-[50px] h-[50px] rounded-full bg-white ${style.balls}`}
-            />
-            <div className={`${style.textBox}`}>
-              <h2 className="font-retro text-2xl">UNITED RHTYMS UKM PARADE</h2>
-              <small className="font-poppins">10 September 2025</small>
-              <p className="font-poppins mt-3">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-              </p>
-              <span className={`${style.arrowLeft}`}></span>
-            </div>
-          </div>
-          <div className={`${style.container} ${style.rightContainer}`}>
-            <div
-              className={`w-[50px] h-[50px] rounded-full bg-white ${style.balls}`}
-            />
-            <div className={`${style.textBox}`}>
-              <h2 className="font-retro text-2xl">UNITED RHTYMS UKM PARADE</h2>
-              <small className="font-poppins">10 September 2025</small>
-              <p className="font-poppins mt-3">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-              </p>
-              <span className={`${style.arrowRight}`}></span>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
