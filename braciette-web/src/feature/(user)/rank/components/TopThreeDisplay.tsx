a28667b0b0be1e6863e938f852ef59ec017844fb
tristan <tristan.alhabas@gmail.com>
2025-09-26 10:58:54 +0700

    feat: Responsive Adjustments <Rank Page>

--- braciette-web/src/feature/(user)/rank/components/TopThreeDisplay.tsx
+++ braciette-web/src/feature/(user)/rank/components/TopThreeDisplay.tsx
@@ -11,33 +11,61 @@ export function TopThreeDisplay({ data }: TopThreeDisplayProps) {
   const rank3 = data.find((item) => item.rank === 3);
 
   return (
-    <div className="relative flex justify-center items-end gap-16 mt-12 w-full">
-      {/* Rank 2 */}
-      {rank2 && (
-        <CardRank
-          rank={rank2.rank}
-          orgName={rank2.organization.name}
-          orgLogo={rank2.organization.logoUrl}
-        />
-      )}
+    <>
+      <div className="hidden lg:flex relative justify-center items-end gap-16 mt-12 w-full">
+        {/* Rank 2 */}
+        {rank2 && (
+          <CardRank
+            rank={rank2.rank}
+            orgName={rank2.organization.name}
+            orgLogo={rank2.organization.logoUrl}
+          />
+        )}
 
-      {rank1 && (
-        <div className="mb-16">
+        {rank1 && (
+          <div className="mb-16">
+            <CardRank
+              rank={rank1.rank}
+              orgName={rank1.organization.name}
+              orgLogo={rank1.organization.logoUrl}
+            />
+          </div>
+        )}
+
+        {rank3 && (
+          <CardRank
+            rank={rank3.rank}
+            orgName={rank3.organization.name}
+            orgLogo={rank3.organization.logoUrl}
+          />
+        )}
+      </div>
+      <div className="relative flex lg:hidden flex-col justify-center items-center gap-16 mt-12 w-full">
+        {rank1 && (
           <CardRank
             rank={rank1.rank}
             orgName={rank1.organization.name}
             orgLogo={rank1.organization.logoUrl}
           />
-        </div>
-      )}
+        )}
 
-      {rank3 && (
-        <CardRank
-          rank={rank3.rank}
-          orgName={rank3.organization.name}
-          orgLogo={rank3.organization.logoUrl}
-        />
-      )}
-    </div>
+        {/* Rank 2 */}
+        {rank2 && (
+          <CardRank
+            rank={rank2.rank}
+            orgName={rank2.organization.name}
+            orgLogo={rank2.organization.logoUrl}
+          />
+        )}
+
+        {rank3 && (
+          <CardRank
+            rank={rank3.rank}
+            orgName={rank3.organization.name}
+            orgLogo={rank3.organization.logoUrl}
+          />
+        )}
+      </div>
+    </>
   );
 }
